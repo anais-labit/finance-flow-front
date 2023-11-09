bcc1f79f6a156b0260220237f32e9305b5131a6d
anais-labit <anais.labit@gmail.com>
2023-11-09 11:06:05 +0100

    login fonctionnel

--- finance-flow-front/src/App.jsx
+++ finance-flow-front/src/App.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Dashboard from './components/Dashboard';
-import TransactionForm from './components/TransactionForm';
-import TransactionList from './components/TransactionList';
-// import Login from './components/Login';
-import { FinanceProvider } from './contexts/FinanceContext';
-import './App.css';
-import './index.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import TransactionForm from "./components/TransactionForm";
+import TransactionList from "./components/TransactionList";
+import LoginForm from "./components/LoginForm";
+import { FinanceProvider } from "./contexts/FinanceContext";
+import "./App.css";
+import "./index.css";
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [welcomeLogin, setWelcomeLogin] = useState("");
+
   return (
     <FinanceProvider>
       <div className="App">
         <Navbar />
         <main>
-          {/* <Login /> */}
-          <Dashboard />
-          <TransactionForm />
-          <TransactionList />
+          {!isConnected ? (
+            <LoginForm
+              setIsConnected={setIsConnected}
+              welcomeLogin={welcomeLogin}
+            />
+          ) : (
+            <>
+              <Dashboard />
+              <TransactionForm />
+              <TransactionList />
+            </>
+          )}
         </main>
       </div>
     </FinanceProvider>
---
+++ finance-flow-front/src/components/LoginForm.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "../assets/css/Login.css";
+
+function LoginForm({ setIsConnected }) {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLogin() {
+    let data = new FormData();
+    data.append("login", login);
+    data.append("password", password);
+    data.append("submitLoginForm", "");
+
+    let response = await fetch(
+      "http://localhost/plateforme/finance-flow-back/index.php",
+      {
+        method: "POST",
+        body: data,
+        mode: "cors",
+      }
+    );
+    let jsonResponse = await response.text();
+    // console.log(jsonResponse);
+    setIsConnected(true);
+  }
+
+  return (
+    <div className="login-form">
+      <form id="loginForm" action="" method="post">
+        <h2 className="text-center">Connexion</h2>
+        <p id="message"></p>
+        <div className="form-group">
+          <label htmlFor="login" id="login"></label>
+          <input
+            type="text"
+            name="login"
+            className="form-control"
+            placeholder="Login"
+            required="required"
+            autoComplete="off"
+            onChange={(e) => {
+              setLogin(e.target.value), console.log(login);
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" id="password"></label>
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            placeholder="Mot de passe"
+            required="required"
+            autoComplete="off"
+            onChange={(e) => {
+              setPassword(e.target.value), console.log(password);
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <button
+            type="button"
+            name="submit"
+            className="btn btn-primary btn-block"
+            id="signInBtn"
+            onClick={handleLogin}
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+      <p className="text-center">
+        <a href="inscription.php">Sign Up</a>
+      </p>
+    </div>
+  );
+}
+
+export default LoginForm;
