e70bc0a0fd8ad6a5105a0774a870973018adfc8f
romain-chazaut <romain.chazaut@laplateforme.io>
2023-11-10 15:27:06 +0100

    burgerMenu in function

--- finance-flow-front/src/components/Dashboard.jsx
+++ finance-flow-front/src/components/Dashboard.jsx
@@ -1,18 +1,30 @@
-// Dashboard.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import CategorySummary from './CategorySummary';
 import BudgetTracker from './BudgetTracker';
-import AccountSummary from './AccountSummary'; // Assurez-vous d'avoir ce composant
+import AccountSummary from './AccountSummary';
+import BurgerMenu from './BurgerMenu';
 import '../assets/css/Dashboard.css';
 
 function Dashboard() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <section className="dashboard">
       <h2>Dashboard</h2>
-      <AccountSummary /> {/* Ajoutez le composant AccountSummary ici */}
-      <CategorySummary />
-      <BudgetTracker />
-      {/* Vous pouvez ajouter d'autres composants de tableau de bord ici si nécessaire */}
+      <AccountSummary /> {/* Affichage du solde général */}
+
+      <BurgerMenu onClick={toggleMenu} /> {/* Bouton du menu burger */}
+
+      {isMenuOpen && (
+        <>
+          <CategorySummary />
+          <BudgetTracker />
+        </>
+      )}
     </section>
   );
 }
