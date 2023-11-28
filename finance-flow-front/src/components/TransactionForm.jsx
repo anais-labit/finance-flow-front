c164512ea4d6324c211ace794a2e75aae871475e
romain-chazaut <romain.chazaut@laplateforme.io>
2023-11-28 17:15:08 +0100

    wip: burger menu addTransaction

--- finance-flow-front/src/components/TransactionForm.jsx
+++ finance-flow-front/src/components/TransactionForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { FinanceContext } from '../contexts/FinanceContext';
 import '../assets/css/TransactionForm.css';
+import '../assets/css/BurgerMenu.css';
 
 function TransactionForm() {
   const { addTransaction } = useContext(FinanceContext);
@@ -10,45 +11,58 @@ function TransactionForm() {
     amount: 0,
     category: '',
   });
+  const [showForm, setShowForm] = useState(false); // État pour contrôler l'affichage du formulaire
 
   const handleSubmit = (event) => {
     event.preventDefault();
     addTransaction(transaction);
-    setTransaction({ date: '', title: '', amount: 0, category: '' }); // Reset form
+    setTransaction({ date: '', title: '', amount: 0, category: '' });
+    setShowForm(false); // Fermer le formulaire après l'ajout
+  };
+
+  const handleBurgerMenuClick = () => {
+    setShowForm(!showForm); // Basculer l'affichage du formulaire
   };
 
   return (
-    <form onSubmit={handleSubmit} className="transaction-form">
-      <input
-        type="date"
-        value={transaction.date}
-        onChange={(e) => setTransaction({ ...transaction, date: e.target.value })}
-        required
-      />
-      <input
-        type="text"
-        value={transaction.title}
-        onChange={(e) => setTransaction({ ...transaction, title: e.target.value })}
-        placeholder="Title"
-        required
-      />
-      <input
-        type="number"
-        value={transaction.amount}
-        onChange={(e) => setTransaction({ ...transaction, amount: e.target.value })}
-        placeholder="Amount"
-        required
-      />
-      <select
-        value={transaction.category}
-        onChange={(e) => setTransaction({ ...transaction, category: e.target.value })}
-        required
-      >
-        <option value="">Select Category</option>
-        {/* Map through categories here */}
-      </select>
-      <button type="submit">Add Transaction</button>
-    </form>
+    <>
+      <button className="burger-menu" onClick={handleBurgerMenuClick}>
+        ☰ Add Transaction
+      </button>
+      {showForm && (
+        <form onSubmit={handleSubmit} className="transaction-form">
+          <input
+            type="date"
+            value={transaction.date}
+            onChange={(e) => setTransaction({ ...transaction, date: e.target.value })}
+            required
+          />
+          <input
+            type="text"
+            value={transaction.title}
+            onChange={(e) => setTransaction({ ...transaction, title: e.target.value })}
+            placeholder="Title"
+            required
+          />
+          <input
+            type="number"
+            value={transaction.amount}
+            onChange={(e) => setTransaction({ ...transaction, amount: e.target.value })}
+            placeholder="Amount"
+            required
+          />
+          <select
+            value={transaction.category}
+            onChange={(e) => setTransaction({ ...transaction, category: e.target.value })}
+            required
+          >
+            <option value="">Select Category</option>
+            {/* Ici, vous pouvez mapper vos catégories */}
+          </select>
+          <button type="submit">Add Transaction</button>
+        </form>
+      )}
+    </>
   );
 }
 
